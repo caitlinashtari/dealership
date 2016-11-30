b93dfb3bb10a5f8c5d308ffc14604718fbd1edb5
caitlin ashtari and kenneth black <caitlin@>
2016-11-30 10:25:09 -0800

    add dealership class with 11 passing unit tests.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,17 +1,28 @@
 class Vehicle
   @@vehicles = []
-  attr_reader(:make, :model, :year, :id, :vehicles)
+  attr_reader(:make, :model, :year)
+
   define_method(:initialize) do |attributes|
     @make = attributes.fetch(:make)
     @model = attributes.fetch(:model)
     @year = attributes.fetch(:year)
-    @id = @@vehicles.length().+(1)
-    @@vehicles.push(self)
   end
   define_singleton_method(:clear) do
     @@vehicles = []
   end
-  define_singleton_method(:all_vehicles) do
+  define_singleton_method(:all) do
     @@vehicles
+  end
+  define_method(:save) do
+    @@vehicles.push(self)
+  end
+  define_method(:age) do
+    current_year = Time.new().year()
+    current_year.-(@year)
+  end
+
+  define_method(:worth_buying?) do
+    american_cars = ["Chrysler", "Ford", "GM"]
+    american_cars.include?(@make).&(self.age().<=(15))
   end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,33 +2,34 @@ require('rspec')
 require('vehicle')
 
 describe(Vehicle) do
+  test_vehicle = Vehicle.new({:make => "Toyota", :model => "Prius", :year => 2016})
+  test_vehicle2 = Vehicle.new({:make => "Toyota", :model => "Yaris", :year => 2008})
+
   before() do
     Vehicle.clear()
   end
 
   describe('#initialize') do
     it('sets the vehicle attributes') do
-      test_vehicle = Vehicle.new({:make => "Toyota", :model => "Prius", :year => 2016})
       expect(test_vehicle.make).to(eq("Toyota"))
       expect(test_vehicle.model).to(eq("Prius"))
       expect(test_vehicle.year).to(eq(2016))
-      expect(test_vehicle.id).to(eq(1))
-      test_vehicle2 = Vehicle.new({:make => "Toyota", :model => "Yaris", :year => 2008})
-      expect(test_vehicle2.id).to(eq(2))
     end
   end
-  describe(".all_vehicles") do
+  describe(".all") do
     it('returns a list of all the vehicles') do
-        expect(Vehicle.all_vehicles).to(eq([]))
+        expect(Vehicle.all).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it('saves the vehicle') do
+      test_vehicle.save
+      expect(Vehicle.all).to eq([test_vehicle])
     end
   end
   describe(".clear") do
     it('clears the lot') do
-      test_vehicle = Vehicle.new({:make => "Toyota", :model => "Prius", :year => 2016})
-      Vehicle.clear
-      expect(Vehicle.all_vehicles).to eq []
+      expect(Vehicle.all).to eq []
     end
   end
-
-  
 end
