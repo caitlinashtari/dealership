b866ac8f3907f830293c1534598b10f3b1610da0
caitlin ashtari and kenneth black <kenneth@>
2016-11-30 16:24:25 -0800

    finish front end mvp, needs more integration tests

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,6 +1,6 @@
 class Vehicle
   @@vehicles = []
-  attr_reader(:make, :model, :year)
+  attr_reader(:make, :model, :year, :id)
 
   define_method(:initialize) do |attributes|
     @make = attributes.fetch(:make)
@@ -15,6 +15,7 @@ class Vehicle
   end
   define_method(:save) do
     @@vehicles.push(self)
+    @id = @@vehicles.length
   end
   define_method(:age) do
     current_year = Time.new().year()
@@ -24,5 +25,15 @@ class Vehicle
   define_method(:worth_buying?) do
     american_cars = ["Chrysler", "Ford", "GM"]
     american_cars.include?(@make).&(self.age().<=(15))
+  end
+
+  define_singleton_method(:find) do |id|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id.eql? id
+        found_vehicle = vehicle
+      end
+    end
+    found_vehicle
   end
 end
